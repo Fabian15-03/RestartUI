c61ffcc091f98efb2083bde75d3b8838f3d7bc37
Fabian15-03 <luminition@hotmail.com>
2023-05-28 13:18:14 -0500

    Initial Commit

---
+++ RestartUI/RestartUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartUIApp.swift
+//  RestartUI
+//
+//  Created by Fabian Gonzales  on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
